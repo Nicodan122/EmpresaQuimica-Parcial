bda65934e76531c4bf71caa9d06a7c0254e6f21c
Henry <115681206+NyehrQ@users.noreply.github.com>
2024-11-23 11:35:54 -0300

    Test2

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 HEADERS = { 'X-Master-Key' : '$2a$10$CxhpjsSuncnI69.51ic/0.fJ4IwVeURoTQP9wfCtQjB/8GLjC627S' }
 
-<<<<<<< Updated upstream
-JSONBIN_URL_USUARIOS = 'https://api.jsonbin.io/v3/b/671a4cbae41b4d34e448174a'
-JSONBIN_URL_PRODUCTOS = 'https://api.jsonbin.io/v3/b/671a4cf4acd3cb34a89c4dc7'
-JSONBIN_URL_VENTA = 'https://api.jsonbin.io/v3/b/671a4d11ad19ca34f8bdd7c3'
-JSONBIN_URL_VENTA_DETALLE = 'https://api.jsonbin.io/v3/b/671a4d33e41b4d34e4481777'
-=======
 JSONBIN_URL_USUARIOS = 'https://api.jsonbin.io/v3/b/6741d993e41b4d34e4593a06'
 JSONBIN_URL_PRODUCTOS = 'https://api.jsonbin.io/v3/b/6741d95ead19ca34f8cef67c'
 JSONBIN_URL_VENTA = 'https://api.jsonbin.io/v3/b/6741d9bcad19ca34f8cef6a4'
 JSONBIN_URL_VENTA_DETALLE = 'https://api.jsonbin.io/v3/b/6741dec2e41b4d34e4593be6'
->>>>>>> Stashed changes
+
